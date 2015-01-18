1b4453a689907e897ed935d582837e03b6ba19d8
francois-xavier <frxpoulin@gmail.com>
2015-01-17 20:08:31 -0500

    test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/instant-api');
+// module.exports = require('./lib/instant-api');
+var test = require('./lib/instant-api');
+
+
+var minus = function(a, b, callback) {
+	require('q');
+	callback(a - b);
+};
+
+test.config();
+
+var cloudedMinus = test.createApi(minus, ['fs', 'q'], {
+	name: 'MINUS'
+});
+
